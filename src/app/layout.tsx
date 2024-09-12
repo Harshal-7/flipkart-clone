3fbf957b1f5e506880e8e15a6b8657db1f2f1a3c
Harshal Shinde <shindeharshal009@gmail.com>
2024-09-13 01:18:29 +0530

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,14 @@ import Header from "@/components/Header";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import StoreProvider from "./StoreProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Flipkart Clone",
+  description:
+    "This is a flipkart clone where user can search for products, see product details page, add products to cart and wishlist with secure authentication and authorization",
 };
 
 export default function RootLayout({
@@ -30,6 +33,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </StoreProvider>
+        <Analytics />
       </body>
     </html>
   );
